fc8763177d6bce0d8c02aeb012227d0c61687352
Unknown <kovacs.zoltan195@gmail.com>
2017-10-21 10:51:11 +0200

    toast

--- app/src/main/java/com/example/kovacszoltan/rock_scissors_paper/MainActivity.java
+++ app/src/main/java/com/example/kovacszoltan/rock_scissors_paper/MainActivity.java
@@ -6,10 +6,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 import java.util.Random;
+
+import static android.R.attr.duration;
 
 public class MainActivity extends AppCompatActivity {
     private Button btn_ko;
@@ -42,14 +45,20 @@ public class MainActivity extends AppCompatActivity {
                 switch(gep_valasztasa){
                     case 1:
                         robot.setBackgroundResource(R.drawable.rock);
+                        Toast toast=Toast.makeText(getApplicationContext(),"A két kő egyenlően erős",Toast.LENGTH_SHORT);
+                        toast.show();
                         break;
                     case 2:
                         robot.setBackgroundResource(R.drawable.paper);
                         ember_pont--;
+                        Toast toastpaper=Toast.makeText(getApplicationContext(),"A papír erősebb a kőnél.",Toast.LENGTH_SHORT);
+                        toastpaper.show();
                         break;
                     case 3:
                         robot.setBackgroundResource(R.drawable.scissors);
                         robot_pont++;
+                        Toast toastollo=Toast.makeText(getApplicationContext(),"A kő erősebb az ollónál",Toast.LENGTH_SHORT);
+                        toastollo.show();
                         break;
                 }
                 eredmeny.setText(String.valueOf(ember_pont) + ':' + String.valueOf(robot_pont));
@@ -65,13 +74,19 @@ public class MainActivity extends AppCompatActivity {
                     case 1:
                         robot.setBackgroundResource(R.drawable.rock);
                         robot_pont--;
+                        Toast toastkoollo=Toast.makeText(getApplicationContext(),"A kő erősebb az ollónál",Toast.LENGTH_SHORT);
+                        toastkoollo.show();
                         break;
                     case 2:
                         robot.setBackgroundResource(R.drawable.paper);
                         ember_pont++;
+                        Toast toastollopapir=Toast.makeText(getApplicationContext(),"Az olló erősebb a papírnál",Toast.LENGTH_SHORT);
+                        toastollopapir.show();
                         break;
                     case 3:
                         robot.setBackgroundResource(R.drawable.scissors);
+                        Toast toast=Toast.makeText(getApplicationContext(),"A két olló egyenlően erős",Toast.LENGTH_SHORT);
+                        toast.show();
                         break;
                 }
                 eredmeny.setText(String.valueOf(ember_pont) + ':' + String.valueOf(robot_pont));
@@ -87,15 +102,19 @@ public class MainActivity extends AppCompatActivity {
                     case 1:
                         robot.setBackgroundResource(R.drawable.rock);
                         ember_pont++;
-
+                        Toast toast=Toast.makeText(getApplicationContext(),"A papír erősebb a kőnél",Toast.LENGTH_SHORT);
+                        toast.show();
                         break;
                     case 2:
                         robot.setBackgroundResource(R.drawable.paper);
-
+                        Toast toastpapir=Toast.makeText(getApplicationContext(),"A két papir egyenlően erős",Toast.LENGTH_SHORT);
+                        toastpapir.show();
                         break;
                     case 3:
                         robot.setBackgroundResource(R.drawable.scissors);
                         robot_pont--;
+                        Toast toastollo=Toast.makeText(getApplicationContext(),"Az olló erősebb a papírnál",Toast.LENGTH_SHORT);
+                        toastollo.show();
                         break;
                 }
                 eredmeny.setText(String.valueOf(ember_pont) + ':' + String.valueOf(robot_pont));
